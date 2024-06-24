8b6e2b6fdfe933f05c7a657b633194420f3dfac8
charm <charmniovi@tutanota.com>
2024-06-24 08:00:10 +0000

    solution

--- app/main.py
+++ app/main.py
@@ -2,7 +2,38 @@ import json
 import xml.etree.ElementTree as ET
 
 
-class Book:
+class ConsoleBook:
+    def display_console(self):
+        print(self.content)
+
+    def print_console(self):
+        print(f"Printing the book: {self.title}...")
+        print(self.content)
+
+
+class ReverseBook:
+    def print_reverse(self):
+        print(f"Printing the book in reverse: {self.title}...")
+        print(self.content[::-1])
+
+    def display_reverse(self):
+        print(self.content[::-1])
+
+
+class SerializeBook:
+    def serialize_json(self):
+        return json.dumps({"title": self.title, "content": self.content})
+
+    def serialize_xml(self):
+        root = ET.Element("book")
+        title = ET.SubElement(root, "title")
+        title.text = self.title
+        content = ET.SubElement(root, "content")
+        content.text = self.content
+        return ET.tostring(root, encoding="unicode")
+
+
+class Book(ConsoleBook, ReverseBook, SerializeBook):
     def __init__(self, title: str, content: str):
         self.title = title
         self.content = content
@@ -17,35 +48,12 @@ class Book:
     def get_spicific_method(cls, cmd: str, method_type: str) -> list:
         return [i for i in dir(cls) if cmd in i and method_type in i]
 
-    def display_console(self):
-        print(self.content)
-
-    def print_console(self):
-        print(f"Printing the book: {self.title}...")
-        print(self.content)
-
-    def print_reverse(self):
-        print(f"Printing the book in reverse: {self.title}...")
-        print(self.content[::-1])
-
-    def display_reverse(self):
-        print(self.content[::-1])
-
-    def serialize_json(self):
-        return json.dumps({"title": self.title, "content": self.content})
-
-    def serialize_xml(self):
-        root = ET.Element("book")
-        title = ET.SubElement(root, "title")
-        title.text = self.title
-        content = ET.SubElement(root, "content")
-        content.text = self.content
-        return ET.tostring(root, encoding="unicode")
-
 
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
     for cmd, method_type in commands:
-        book.chose_method(cmd, method_type)
+        i = book.chose_method(cmd, method_type)
+        if i:
+            return i
 
 
 if __name__ == "__main__":
